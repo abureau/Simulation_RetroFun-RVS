0b3726e6e6c96ebc218d57ff42d8e6f2d4ed3f71
Loïc Mangnier <loicm@cedar5.cedar.computecanada.ca>
2022-02-03 07:34:56 -0800

    Config Files + Script simulating variants

---
+++ simulate_variants.R
@@ -0,0 +1,97 @@
+#!/usr/bin/env Rscript
+cat("The script is launching...\n")
+suppressPackageStartupMessages(library(GenomicRanges))
+suppressPackageStartupMessages(library(rtracklayer))
+
+#The random seed is generated randomly to add sample noise at each generation
+set.seed(sample(1:1000,1))
+
+args = commandArgs(trailingOnly=TRUE)
+prob_causal = as.numeric(args[3])
+
+cat("Import variant file \n")
+#Variants within the region are considered
+#TAD: chr1-Chr1:24100000-24970000
+
+variants = read.table(args[1], header=TRUE, sep="\t")
+colnames(variants) = c("chr", "pos", "ref","alt", "maf")
+
+variants = variants[variants$maf<=0.01,]
+MAF = do.call("rbind",strsplit(variants$maf,",", fixed=T))
+
+MAF = apply(MAF,2, as.numeric)
+variants$maf = apply(MAF,1,min)
+
+n_variants = nrow(variants)
+
+
+cat("Import CRH file\n")
+#GRange elements in bed format
+GRange_CRHs = import(args[2], format="bed")
+GRange_variants = GRanges(seqnames=paste0("chr",variants$chr), ranges=IRanges(start=variants$pos,end=variants$pos))
+
+variants_tmp = variants
+
+overlaps_variants_CRHs = countOverlaps(GRange_variants, GRange_CRHs)
+overlaps_variants_CRHs[overlaps_variants_CRHs>1] = 1 
+
+variants_in_CRHs = findOverlaps(GRange_variants, GRange_CRHs)
+index_variants_in_CRHs = unique(queryHits(variants_in_CRHs))
+
+n_variants_in_CRHs = length(overlaps_variants_CRHs[overlaps_variants_CRHs==1])
+
+variants_tmp[index_variants_in_CRHs,"inCRHs"] = TRUE
+index_variants_out_CRHs = which(is.na(variants_tmp$inCRHs))
+
+#Causal variants are generated based on the probability
+#In the entire regions X% of variants will be causal
+causal_variants = sample(0:1, nrow(variants_tmp), replace=T, prob=c((1-prob_causal),prob_causal))
+n_causal_variants = length(causal_variants[causal_variants==1])
+
+cat("Scenario 1 \n")
+#Scenario 1 : 100%
+#All the causal variants are located within CRH elements 
+variants_tmp[sample(index_variants_in_CRHs, n_causal_variants, replace=F), "Scenario_100"] = 1
+
+cat("Scenario 2 \n")
+#Scenario 2 : 75%
+#75% of causal variants are located within CRHs the rest are outside CRH elements
+n_causal_in_CRHs = floor(n_causal_variants*0.75)
+n_causal_out_CRHs = floor(n_causal_variants*0.25)
+
+if((length(n_causal_in_CRHs) + length(n_causal_out_CRHs))!=n_causal_variants) n_causal_in_CRHs = n_causal_in_CRHs +1
+
+index_causal_in_CRHs = sample(index_variants_in_CRHs, n_causal_in_CRHs , replace=F)
+index_causal_out_CRHs = sample(index_variants_out_CRHs, n_causal_out_CRHs , replace=F)
+
+variants_tmp[c(index_causal_in_CRHs, index_causal_out_CRHs), "Scenario_75"] = 1
+
+cat("Scenario 3 \n")
+#Scenario 3 : 50%
+#Equal part of causal variants within and outside CRH elements
+n_causal_in_CRHs = floor(n_causal_variants*0.50)
+n_causal_out_CRHs = floor(n_causal_variants*0.50)
+
+if((length(n_causal_in_CRHs) + length(n_causal_out_CRHs))!=n_causal_variants) n_causal_in_CRHs = n_causal_in_CRHs + 1
+
+index_causal_in_CRHs = sample(index_variants_in_CRHs, n_causal_in_CRHs , replace=F)
+index_causal_out_CRHs = sample(index_variants_out_CRHs, n_causal_out_CRHs , replace=F)
+
+variants_tmp[c(index_causal_in_CRHs, index_causal_out_CRHs), "Scenario_50"] = 1
+
+
+variants_tmp[is.na(variants_tmp$Scenario_100), "Scenario_100"] = 0
+variants_tmp[is.na(variants_tmp$Scenario_75), "Scenario_75"] = 0
+variants_tmp[is.na(variants_tmp$Scenario_50), "Scenario_50"] = 0
+
+
+variants_tmp = cbind("TAD", variants_tmp)
+
+variants_scenario100 = variants_tmp[,c(1,2,3,4,5,6,8)] 
+variants_scenario75 = variants_tmp[,c(1,2,3,4,5,6,9)]
+variants_scenario50 = variants_tmp[,c(1,2,3,4,5,6,10)]
+
+write.table(variants_scenario100, paste0("rare_variants_scenario100_",prob_causal,".sfs"), quote=F, col.names=F, row.names=F)
+write.table(variants_scenario75, paste0("rare_variants_scenario75_",prob_causal,".sfs"), quote=F, col.names=F, row.names=F)
+write.table(variants_scenario50, paste0("rare_variants_scenario50_",prob_causal,".sfs"), quote=F, col.names=F, row.names=F)
+
