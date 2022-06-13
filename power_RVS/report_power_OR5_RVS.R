4b50bed45966d82065bd8dcc29501f4a6fde61ae
abureau <abureau6@gmail.com>
2022-06-13 09:00:10 -0400

    Power of RVS with 1% causal variants

--- power_RVS/report_power_OR5_RVS.R
+++ power_RVS/report_power_OR5_RVS.R
@@ -9,13 +9,17 @@ ACAT.fct = function(vec)
   vec = ifelse(vec>0.999999,1-1/length(vec),vec)
   ACAT(vec)
 }
-# 2% of causal variants
 
 load("OR5_100.02.RData")
 load("OR5_75.02.RData")
 load("OR5_50.02.RData")
+load("OR5_100.01.RData")
+load("OR5_75.01.RData")
+load("OR5_50.01.RData")
 
 # 100% causal variants in CRH 1
+
+# 2% of causal variants
 minp100.2pc.vec = sapply(p100.OR5.02.list,minp_Bonf)
 minpall100.2pc.vec = sapply(pall100.OR5.02.list,minp_Bonf)
 minpall_lf100.2pc.vec = sapply(pall_lf100.OR5.02.list,minp_Bonf)
@@ -27,9 +31,25 @@ ACATpall100.2pc.vec = sapply(pall100.OR5.02.list,ACAT.fct)
 ACATpall_lf100.2pc.vec = sapply(pall_lf100.OR5.02.list,ACAT.fct)
 power100.2pc.05 = c(sum(ACATp100.2pc.vec<0.05),sum(ACATpall100.2pc.vec<0.05),sum(ACATpall_lf100.2pc.vec<0.05))/length(ACATp100.2pc.vec)
 power100.2pc.3000 = c(sum(ACATp100.2pc.vec<0.05/3000),sum(ACATpall100.2pc.vec<0.05/3000),sum(ACATpall_lf100.2pc.vec<0.05/3000))/length(ACATp100.2pc.vec)
+power100.2pc.6000 = c(sum(ACATp100.2pc.vec<0.05/6000),sum(ACATpall100.2pc.vec<0.05/6000),sum(ACATpall_lf100.2pc.vec<0.05/6000))/length(ACATp100.2pc.vec)
 
+# 1% of causal variants
+minp100.1pc.vec = sapply(p100.OR5.01.list,minp_Bonf)
+minpall100.1pc.vec = sapply(pall100.OR5.01.list,minp_Bonf)
+minpall_lf100.1pc.vec = sapply(pall_lf100.OR5.01.list,minp_Bonf)
+#power100.1pc.05 = c(sum(minp100.1pc.vec<0.05),sum(minpall100.1pc.vec<0.05),sum(minpall_lf100.1pc.vec<0.05))
+#power100.1pc.3000 = c(sum(minp100.1pc.vec<0.05/3000),sum(minpall100.1pc.vec<0.05/3000),sum(minpall_lf100.1pc.vec<0.05/3000))
+
+ACATp100.1pc.vec = sapply(p100.OR5.01.list,ACAT.fct)
+ACATpall100.1pc.vec = sapply(pall100.OR5.01.list,ACAT.fct)
+ACATpall_lf100.1pc.vec = sapply(pall_lf100.OR5.01.list,ACAT.fct)
+power100.1pc.05 = c(sum(ACATp100.1pc.vec<0.05),sum(ACATpall100.1pc.vec<0.05),sum(ACATpall_lf100.1pc.vec<0.05))/length(ACATp100.1pc.vec)
+power100.1pc.3000 = c(sum(ACATp100.1pc.vec<0.05/3000),sum(ACATpall100.1pc.vec<0.05/3000),sum(ACATpall_lf100.1pc.vec<0.05/3000))/length(ACATp100.1pc.vec)
+power100.1pc.6000 = c(sum(ACATp100.1pc.vec<0.05/6000),sum(ACATpall100.1pc.vec<0.05/6000),sum(ACATpall_lf100.1pc.vec<0.05/6000))/length(ACATp100.1pc.vec)
 
 # 75% causal variants in CRH 1
+
+# 2% of causal variants
 minp75.2pc.vec = sapply(p75.OR5.02.list,minp_Bonf)
 minpall75.2pc.vec = sapply(pall75.OR5.02.list,minp_Bonf)
 minpall_lf75.2pc.vec = sapply(pall_lf75.OR5.02.list,minp_Bonf)
@@ -41,8 +61,25 @@ ACATpall75.2pc.vec = sapply(pall75.OR5.02.list,ACAT.fct)
 ACATpall_lf75.2pc.vec = sapply(pall_lf75.OR5.02.list,ACAT.fct)
 power75.2pc.05 = c(sum(ACATp75.2pc.vec<0.05),sum(ACATpall75.2pc.vec<0.05),sum(ACATpall_lf75.2pc.vec<0.05))/length(ACATp75.2pc.vec)
 power75.2pc.3000 = c(sum(ACATp75.2pc.vec<0.05/3000),sum(ACATpall75.2pc.vec<0.05/3000),sum(ACATpall_lf75.2pc.vec<0.05/3000))/length(ACATp75.2pc.vec)
+power75.2pc.6000 = c(sum(ACATp75.2pc.vec<0.05/6000),sum(ACATpall75.2pc.vec<0.05/6000),sum(ACATpall_lf75.2pc.vec<0.05/3000))/length(ACATp75.2pc.vec)
+
+# 1% of causal variants
+minp75.1pc.vec = sapply(p75.OR5.01.list,minp_Bonf)
+minpall75.1pc.vec = sapply(pall75.OR5.01.list,minp_Bonf)
+minpall_lf75.1pc.vec = sapply(pall_lf75.OR5.01.list,minp_Bonf)
+#power75.1pc.05 = c(sum(minp75.1pc.vec<0.05),sum(minpall75.1pc.vec<0.05),sum(minpall_lf75.1pc.vec<0.05))
+#power75.1pc.3000 = c(sum(minp75.1pc.vec<0.05/3000),sum(minpall75.1pc.vec<0.05/3000),sum(minpall_lf75.1pc.vec<0.05/3000))
+
+ACATp75.1pc.vec = sapply(p75.OR5.01.list,ACAT.fct)
+ACATpall75.1pc.vec = sapply(pall75.OR5.01.list,ACAT.fct)
+ACATpall_lf75.1pc.vec = sapply(pall_lf75.OR5.01.list,ACAT.fct)
+power75.1pc.05 = c(sum(ACATp75.1pc.vec<0.05),sum(ACATpall75.1pc.vec<0.05),sum(ACATpall_lf75.1pc.vec<0.05))/length(ACATp75.1pc.vec)
+power75.1pc.3000 = c(sum(ACATp75.1pc.vec<0.05/3000),sum(ACATpall75.1pc.vec<0.05/3000),sum(ACATpall_lf75.1pc.vec<0.05/6000))/length(ACATp75.1pc.vec)
+power75.1pc.6000 = c(sum(ACATp75.1pc.vec<0.05/6000),sum(ACATpall75.1pc.vec<0.05/6000),sum(ACATpall_lf75.1pc.vec<0.05/6000))/length(ACATp75.1pc.vec)
 
 # 50% causal variants in CRH 1
+
+# 2% of causal variants
 minp50.2pc.vec = sapply(p50.OR5.02.list,minp_Bonf)
 minpall50.2pc.vec = sapply(pall50.OR5.02.list,minp_Bonf)
 minpall_lf50.2pc.vec = sapply(pall_lf50.OR5.02.list,minp_Bonf)
@@ -54,12 +91,34 @@ ACATpall50.2pc.vec = sapply(pall50.OR5.02.list,ACAT.fct)
 ACATpall_lf50.2pc.vec = sapply(pall_lf50.OR5.02.list,ACAT.fct)
 power50.2pc.05 = c(sum(ACATp50.2pc.vec<0.05),sum(ACATpall50.2pc.vec<0.05),sum(ACATpall_lf50.2pc.vec<0.05))/length(ACATp50.2pc.vec)
 power50.2pc.3000 = c(sum(ACATp50.2pc.vec<0.05/3000),sum(ACATpall50.2pc.vec<0.05/3000),sum(ACATpall_lf50.2pc.vec<0.05/3000))/length(ACATp50.2pc.vec)
+power50.2pc.6000 = c(sum(ACATp50.2pc.vec<0.05/6000),sum(ACATpall50.2pc.vec<0.05/6000),sum(ACATpall_lf50.2pc.vec<0.05/6000))/length(ACATp50.2pc.vec)
+
+# 1% of causal variants
+minp50.1pc.vec = sapply(p50.OR5.01.list,minp_Bonf)
+minpall50.1pc.vec = sapply(pall50.OR5.01.list,minp_Bonf)
+minpall_lf50.1pc.vec = sapply(pall_lf50.OR5.01.list,minp_Bonf)
+#power50.1pc.05 = c(sum(minp50.1pc.vec<0.05),sum(minpall50.1pc.vec<0.05),sum(minpall_lf50.1pc.vec<0.05))
+#power50.1pc.00005 = c(sum(minp50.1pc.vec<0.00005),sum(minpall50.1pc.vec<0.00005),sum(minpall_lf50.1pc.vec<0.00005))
+
+ACATp50.1pc.vec = sapply(p50.OR5.01.list,ACAT.fct)
+ACATpall50.1pc.vec = sapply(pall50.OR5.01.list,ACAT.fct)
+ACATpall_lf50.1pc.vec = sapply(pall_lf50.OR5.01.list,ACAT.fct)
+power50.1pc.05 = c(sum(ACATp50.1pc.vec<0.05),sum(ACATpall50.1pc.vec<0.05),sum(ACATpall_lf50.1pc.vec<0.05))/length(ACATp50.1pc.vec)
+power50.1pc.3000 = c(sum(ACATp50.1pc.vec<0.05/3000),sum(ACATpall50.1pc.vec<0.05/3000),sum(ACATpall_lf50.1pc.vec<0.05/3000))/length(ACATp50.1pc.vec)
+power50.1pc.6000 = c(sum(ACATp50.1pc.vec<0.05/6000),sum(ACATpall50.1pc.vec<0.05/6000),sum(ACATpall_lf50.1pc.vec<0.05/6000))/length(ACATp50.1pc.vec)
 
 power.05 = cbind(power100.2pc.05,power75.2pc.05,power50.2pc.05)
-power.3000 = cbind(power100.2pc.3000,power75.2pc.3000,power50.2pc.3000)
-row.names(power.3000) = row.names(power.05) = c("Partial sharing","Complete sharing","Complete sharing, fixed")
+power.6000 = cbind(power100.2pc.6000,power75.2pc.6000,power50.2pc.6000)
+row.names(power.6000) = row.names(power.05) = c("Partial sharing","Complete sharing","Complete sharing, fixed")
 power.05
-power.3000
+power.6000
+
+power.1pc.05 = cbind(power100.1pc.05,power75.1pc.05,power50.1pc.05)
+power.1pc.6000 = cbind(power100.1pc.6000,power75.1pc.6000,power50.1pc.6000)
+row.names(power.1pc.6000) = row.names(power.1pc.05) = c("Partial sharing","Complete sharing","Complete sharing, fixed")
+power.1pc.05
+power.1pc.6000
+
 
 # Examine number of windows for variable length
 l100.2pc = sapply(p100.02.list,length)
