fdb39d496c53e23b6fdbcdf61a8c044dd14bdd36
lmangnier <loic.mangnier@crchudequebec.ulaval.ca>
2024-04-12 13:26:28 -0400

    clean and comment code_simulation_null.R

--- code/code_simulation_null.R
+++ code/code_simulation_null.R
@@ -1,7 +1,11 @@
+#This code generates data for Type-I error rate 
+#The data produced by this script are available in the repo
+
 library(RetroFunRVS)
 library(ggplot2)
 library(ggpubr)
 
+#Functions for generating fancy qqplots
 gg_qqplot_facet_grid <- function(list_pvalues,ci = 0.95, title="") {
   #n  <- length(ps)
   
@@ -53,19 +57,22 @@ gg_qqplot <- function(ps, ci = 0.95) {
     ylab(log10Po) + theme_bw()
 }
 
-pedfiles_null = list.files("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Last_sim_null_10000reps\\TAD", full.names = T)
+#pedfiles_null = list.files("data\\Last_sim_null_10000reps\\TAD", full.names = T)
 
-load("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\SPAPsimpleprob.RData")
+load("data\\SPAPsimpleprob.RData")
 
-null_without_consanguinity = read.table("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\null_without_consanguinity.txt", header=TRUE)
-null_with_consanguinity = read.table("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\null_with_consanguinity.txt",header = TRUE)
+#Files with expected values, variances and covariances under the null across all the families
+null_without_consanguinity = read.table("data\\null_without_consanguinity.txt", header=TRUE)
+null_with_consanguinity = read.table("data\\null_with_consanguinity.txt",header = TRUE)
 
+#Binary matrices for all the functional annotations
+Z = read.table("data\\Annot_Z_CRHs.mat", header=F)
+Z_SW = read.table("data\\Annot_Z_SW.mat", header=F)
+Z_Pairs = read.table("data\\Annot_Z_pairs.mat", header=F)
+Z_Genes = read.table("data\\Annot_Z_genes.mat", header=F)
 
-Z = read.table("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Annot_Z_CRHs.mat", header=F)
-Z_SW = read.table("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Annot_Z_SW.mat", header=F)
-Z_Pairs = read.table("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Annot_Z_pairs.mat", header=F)
-Z_Genes = read.table("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Annot_Z_genes.mat", header=F)
-
+#This function reproduces the agg.genos.by.fam function 
+#with slight modifications for accomodating synthetic data structures
 agg.geno.by.fam.for.sim = function(pedfile, FamID=NULL){
   p = read.table(pedfile, header = F)
   fam = p[,1:6]
@@ -105,13 +112,14 @@ agg.geno.by.fam.for.sim = function(pedfile, FamID=NULL){
   
 }
 
-pedfiles_agg_null = readRDS("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pedfiles_agg_null_10000reps.RDS")
+pedfiles_agg_null = readRDS("data\\pedfiles_agg_null_10000reps.RDS")
 
 n.unique.config.by.fam = sapply(forsim.N.list, unique)
 prob.sharing.by.famid = lapply(1:length(forsim.pattern.prob.list), function(x) tapply(forsim.pattern.prob.list[[x]], forsim.N.list[[x]], sum))
 
 names(prob.sharing.by.famid) = names(forsim.pattern.prob.list)
 
+#Filter steps for keeping only functional annotations with 5 families minimally
 variants_by_annot_CRHs = apply(Z,2,function(x) which(x!=0))
 variants_by_annot_Pairs = apply(Z_Pairs,2,function(x) which(x!=0))
 variants_by_annot_Genes = apply(Z_Genes,2,function(x) which(x!=0))
@@ -137,14 +145,10 @@ filter_pedfiles_agg_null=lapply(1:10000, function(X){
   filter.rep.n.fam(pedfiles_agg_null[[X]], variants.by.annot)
 })
 
-pipoo = lapply(1:10000, function(x){
-  filter.rep.n.fam(pedfiles_agg_null[[x]], variants_by_annot)
-})
-
-df.n.fam.by.annot = do.call("rbind",pipoo)
+df.n.fam.by.annot = do.call("rbind",filter_pedfiles_agg_null)
 
 
-
+#Generatin p-values for main scenario
 pvalues_null = lapply(1:10000, function(rep) {
   print(rep)
   Z.tmp = Z[,-which(filter.rep.n.fam(pedfiles_agg_null[[rep]], variants_by_annot_CRHs)<=5), drop=FALSE]
@@ -172,9 +176,9 @@ ggpubr::ggarrange(ggpubr::ggarrange(gg_qqplot(sapply(pvalues_null_indep, functio
                                             "Independence"=unlist(sapply(pvalues_null_indep, function(x) x$Score_V5))))+labs(colour="Variant Structure")+theme(legend.position = "bottom"), labels=c("B","C","D","E"),ncol=2, nrow=2), nrow=2)
 
 
-pedfiles.smallped = list.files("D:\\Vraisemblance_retrospective\\Simulation\\data\\Null_smallped\\TAD", full.names = TRUE)
+#pedfiles.smallped = list.files("D:\\Vraisemblance_retrospective\\Simulation\\data\\Null_smallped\\TAD", full.names = TRUE)
 
-
+#Generating p-values for small to moderate pedigrees
 pvalues.smallped.dep = lapply(1:1000, function(rep){
   print(rep)
   Z.tmp = Z[,-which(filter.rep.n.fam(pedfiles.smallped.agg[[rep]], variants_by_annot_CRHs)<=5), drop=FALSE]
@@ -189,8 +193,8 @@ pvalues.smallped.inddep = lapply(1:1000, function(rep){
   RetroFunRVS::RetroFun.RVS(null_without_consanguinity, pedfiles.smallped.agg[[rep]], Z.tmp,rep(1,510), independence = TRUE)
 })
 
-saveRDS(pvalues.smallped.inddep, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_smallped_independence.RDS")
-saveRDS(pvalues.smallped.dep, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_smallped_dependence.RDS")
+saveRDS(pvalues.smallped.inddep, "data\\pvalues_null_smallped_independence.RDS")
+saveRDS(pvalues.smallped.dep, "data\\pvalues_null_smallped_dependence.RDS")
 
 pvalues_null_Pairs = lapply(1:10000, function(rep) {
   print(rep)
@@ -228,19 +232,11 @@ pvalues_null_SW_filtered_30 = lapply(1:10000, function(rep) {
   RetroFunRVS::RetroFun.RVS(null_without_consanguinity, pedfiles_agg_null[[rep]], Z_SW_filtered_30,rep(1,510))
 })
 
-
-sum(sapply(pvalues_null, function(x) x$ACAT)<=0.05)/10000
-sum(sapply(pvalues_null, function(x) x$ACAT)<=0.01)/10000
-sum(sapply(pvalues_null, function(x) x$ACAT)<=0.001)/10000
-sum(sapply(pvalues_null, function(x) x$ACAT)<=0.0001)/10000
-
-which(sapply(pvalues_null_Genes, function(x) x$ACAT)<=0.001)
-
-saveRDS(pvalues_null_SW, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_SW.RDS")
-saveRDS(pvalues_null_Pairs, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_Pairs.RDS")
-saveRDS(pvalues_null_Genes, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_Genes.RDS")
-saveRDS(pvalues_null, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_CRHs.RDS")
-saveRDS(pvalues_null_indep, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_CRHs_indep.RDS")
+saveRDS(pvalues_null_SW, "data\\pvalues_null_SW.RDS")
+saveRDS(pvalues_null_Pairs, "data\\pvalues_null_Pairs.RDS")
+saveRDS(pvalues_null_Genes, "data\\pvalues_null_Genes.RDS")
+saveRDS(pvalues_null, "data\\pvalues_null_CRHs.RDS")
+saveRDS(pvalues_null_indep, "data\\pvalues_null_CRHs_indep.RDS")
 
 ggarrange(gg_qqplot(sapply(1:10000, function(x) pvalues_null_Pairs[[x]]$ACAT)),
           gg_qqplot(sapply(1:10000, function(x) pvalues_null_Genes[[x]]$ACAT)),
@@ -248,12 +244,12 @@ ggarrange(gg_qqplot(sapply(1:10000, function(x) pvalues_null_Pairs[[x]]$ACAT)),
           labels = c("A","B","C"), ncol=3,nrow=1)
 
 #Analysis by pedigree structure
-pedfiles_null_two_affected = list.files("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Null_two_affected_1000reps_concat", full.names = TRUE)
-pedfiles_null_three_affected = list.files("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Null_three_affected_1000reps_concat", full.names = TRUE)
-pedfiles_null_four_affected = list.files("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Null_four_affected_1000reps_concat", full.names = TRUE)
-pedfiles_null_five_affected = list.files("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Null_five_affected_1000reps_concat", full.names = TRUE)
-pedfiles_null_six_affected = list.files("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Null_six_affected_1000reps_concat", full.names = TRUE)
-pedfiles_null_seven_affected = list.files("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Null_seven_affected_1000reps_concat", full.names = TRUE)
+pedfiles_null_two_affected = list.files("data\\Null_two_affected_1000reps_concat", full.names = TRUE)
+pedfiles_null_three_affected = list.files("data\\Null_three_affected_1000reps_concat", full.names = TRUE)
+pedfiles_null_four_affected = list.files("data\\Null_four_affected_1000reps_concat", full.names = TRUE)
+pedfiles_null_five_affected = list.files("data\\Null_five_affected_1000reps_concat", full.names = TRUE)
+pedfiles_null_six_affected = list.files("data\\Null_six_affected_1000reps_concat", full.names = TRUE)
+pedfiles_null_seven_affected = list.files("data\\Null_seven_affected_1000reps_concat", full.names = TRUE)
 
 
 
@@ -352,45 +348,10 @@ gg_qqplot_facet_grid(list("2 affected"=sapply(p.values.two.affected, function(x)
 
 
 #Analysis with consanguinity
-pedfiles_null_consanguinity = list.files("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Null_consanguinity_concat", full.names = TRUE)
+pedfiles_null_consanguinity = list.files("data\\Null_consanguinity_concat", full.names = TRUE)
 
 #Analysis with only consanguinity 
-pedfiles_null_only_consanguinity = list.files("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\Null_ped_only_consanguinity_concat", full.names = TRUE)
-
-
-p = read.table(pedfiles_null_only_consanguinity[[1]], header = F)
-fam = p[,1:6]
-#Need to be changed by 1 for certain scenarios
-affected = which(fam$V6==1)
-genos = p[,7:ncol(p)]
-
-genos[genos==1] = 0
-genos[genos==2] = 1
-
-df.genos = data.frame(do.call("cbind",lapply(seq(2, ncol(genos),2), function(x){
-  rowSums(genos[,c(x-1,x)])
-})))
-
-df.genos.affected = df.genos[affected,]
-df.genos.affected = data.frame(t(unique(t(df.genos.affected))))
-# if(length(which(colSums(df.genos.affected==2)>0))>0){
-#   df.genos.affected = df.genos.affected[,-which(colSums(df.genos.affected==2)>0)]
-# } else{
-#   df.genos.affected = df.genos.affected
-# }
-df.genos.affected[df.genos.affected==2] = 1
-df.genos.affected = df.genos.affected[,-which(colSums(df.genos.affected)==0)]
-
-df.genos.affected$pedigree = fam[affected,"V1"]
-
-df.genos.agg.by.fam = aggregate(.~pedigree,df.genos.affected, sum)
-
-index_null_fam = which(rowSums(df.genos.agg.by.fam[,-1])==0)
-if(length(index_null_fam) > 0) df.genos.agg.by.fam = df.genos.agg.by.fam[-index_null_fam,]
-
-df.genos.agg.by.fam$pedigree = ifelse(df.genos.agg.by.fam$pedigree %in% null_with_consanguinity$FamID, df.genos.agg.by.fam$pedigree,sub('^[a-z]', '', df.genos.agg.by.fam$pedigree))
-
-
+pedfiles_null_only_consanguinity = list.files("data\\Null_ped_only_consanguinity_concat", full.names = TRUE)
 
 pedfiles_agg_null_only_consanguinity = lapply(1:1000, function(rep) {
   print(rep)
@@ -454,72 +415,69 @@ gg_qqplot_facet_grid(list("Corrected" = sapply(pvalues.null.with.only.consanguin
                                     "Uncorrected"=sapply(pvalues.null.with.only.consanguinity.uncorrected, function(x) x$ACAT)))+labs(colour="Correction")
 
 
-saveRDS(pvalues.null.with.only.consanguinity.uncorrected, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_only_consanguinity_uncorrected.RDS")
-saveRDS(pvalues.null.with.only.consanguinity.corrected, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_only_consanguinity_corrected.RDS")
-saveRDS(pvalues.null.with.consanguinity.corrected, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_consanguinity_corrected.RDS")
-saveRDS(pvalues.null.with.consanguinity.unccorrected, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_only_consanguinity_uncorrected.RDS")
+saveRDS(pvalues.null.with.only.consanguinity.uncorrected, "data\\pvalues_null_only_consanguinity_uncorrected.RDS")
+saveRDS(pvalues.null.with.only.consanguinity.corrected, "data\\pvalues_null_only_consanguinity_corrected.RDS")
+saveRDS(pvalues.null.with.consanguinity.corrected, "data\\pvalues_null_consanguinity_corrected.RDS")
+saveRDS(pvalues.null.with.consanguinity.unccorrected, "data\\pvalues_null_only_consanguinity_uncorrected.RDS")
 
 
-saveRDS(p.values.two.affected, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_two_affected.RDS")
-saveRDS(p.values.three.affected, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_three_affected.RDS")
-saveRDS(p.values.four.affected, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_four_affected.RDS")
-saveRDS(p.values.five.affected, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_five_affected.RDS")
-saveRDS(p.values.six.affected, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_six_affected.RDS")
-saveRDS(p.values.seven.affected, "C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pvalues_null_seven_affected.RDS")
+saveRDS(p.values.two.affected, "data\\pvalues_null_two_affected.RDS")
+saveRDS(p.values.three.affected, "data\\pvalues_null_three_affected.RDS")
+saveRDS(p.values.four.affected, "data\\pvalues_null_four_affected.RDS")
+saveRDS(p.values.five.affected, "data\\pvalues_null_five_affected.RDS")
+saveRDS(p.values.six.affected, "data\\pvalues_null_six_affected.RDS")
+saveRDS(p.values.seven.affected, "data\\pvalues_null_seven_affected.RDS")
 
-load("C:\\Users\\loicm\\Desktop\\Retrospective_LL\\data\\pedsimple.RData")
-library(kinship2)
 
-unique(pedsimple$famid)
+#Code for producing bootstrap-based pvalues
+#First as suggested in the bootRetroFunRVS documentation n.unique.config.by.fam and prob.sharing.by.fam have to be in the same order
+n.unique.config.by.fam = lapply(n.unique.config.by.fam, function(fam) sort(fam))
+prob.sharing.by.famid
 
-famid = c("101",
-          "105",
-          "108",
-          "110",
-          "111",
-          "115",
-          "119",
-          "121",
-          "122",
-          "123",
-          "124",
-          "125",
-          "126",
-          "129",
-          "133",
-          "134",
-          "182",
-          "207",
-          "208",
-          "211",
-          "211A",
-          "212",
-          "212A",
-          "217",
-          "218",
-          "220",
-          "223",
-          "226",
-          "227",
-          "228",
-          "230",
-          "233",
-          "234",
-          "235",
-          "236",
-          "238",
-          "239",
-          "245",
-          "250",
-          "S2029"
-)
-for(fam in famid){
-  
-  jpeg(paste0("C:\\Users\\loicm\\Downloads\\ped",fam,".jpg"), width = 389, height = 430)
-  # 2. Create the plot
-  plot(pedsimple[fam], cex=0.5)
-  # 3. Close the file
-  dev.off()
+bootstrap.samples.CRHs = bootRetroFunRVS(agg.genos.by.fam = pedfiles_agg_null[[786]] ,n.unique.config.by.fam = n.unique.config.by.fam,Z_annot = Z,W = rep(1,510),prob.sharing.by.fam = prob.sharing.by.famid,nullval = null_without_consanguinity ,nrep = 1000)
+Stat = compute.Burden.by.Annot(null.value.by.fam = null_without_consanguinity ,aggregate.geno.by.fam = pedfiles_agg_null[[786]] ,Z_annot = Z,W = rep(1,510))$B
+Bootstrap.p = rowMeans(apply(bootstrap.samples.CRHs, 1, function(row) row>=Stat))
+
+#Code for computing average running time for all the functional annotations
+list.time.running = list("CRHs"=c(),"Genes"=c(), "Pairs"=c(), "SW"=c())
+
+for(i in 1:10){
+  print(i)
+  start_time <- Sys.time()
+  bootRetroFunRVS(pedfiles_agg_null[[1000]], n.unique.config.by.fam, Z_annot = Z,W = rep(1,510),prob.sharing.by.fam = prob.sharing.by.famid,nullval = null_without_consanguinity,nrep = 100)
+  end_time <- Sys.time()
+  list.time.running$CRHs = c(list.time.running$CRHs,end_time-start_time)
+}
+
+for(i in 1:10){
+  print(i)
+  start_time <- Sys.time()
+  bootRetroFunRVS(pedfiles_agg_null[[1000]], n.unique.config.by.fam, Z_annot = Z_Genes,W = rep(1,510),prob.sharing.by.fam = prob.sharing.by.famid,nullval = null_without_consanguinity,nrep = 100)
+  end_time <- Sys.time()
+  list.time.running$Genes = c(list.time.running$Genes,end_time-start_time)
 }
 
 
+for(i in 1:10){
+  print(i)
+  start_time <- Sys.time()
+  bootRetroFunRVS(pedfiles_agg_null[[1000]], n.unique.config.by.fam, Z_annot = Z_Pairs,W = rep(1,510),prob.sharing.by.fam = prob.sharing.by.famid,nullval = null_without_consanguinity,nrep = 100)
+  end_time <- Sys.time()
+  list.time.running$Pairs = c(list.time.running$Pairs,end_time-start_time)
+}
+
+
+for(i in 1:10){
+  print(i)
+  start_time <- Sys.time()
+  bootRetroFunRVS(pedfiles_agg_null[[1000]], n.unique.config.by.fam, Z_annot = Z_SW,W = rep(1,510),prob.sharing.by.fam = prob.sharing.by.famid,nullval = null_without_consanguinity,nrep = 100)
+  end_time <- Sys.time()
+  list.time.running$SW = c(list.time.running$SW,end_time-start_time)
+}
+
+
+
+sum(list.time.running$CRHs)*10
+sum(list.time.running$Genes)*10
+sum(list.time.running$Pairs)*10
+sum(list.time.running$SW)*10
